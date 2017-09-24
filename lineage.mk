cd293a6785d8e423222dd7f996c91e07fac12efd
Psycho Mods <nishsin26@gmail.com>
2017-09-24 13:19:30 +0530

    Update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -1,6 +1,6 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from device
 $(call inherit-product, device/leeco/x3/device.mk)
@@ -16,14 +16,10 @@ PRODUCT_NAME := lineage_x3
 PRODUCT_MANUFACTURER := LeEco
 PRODUCT_MODEL := Le 1s
 TARGET_VENDOR := LeEco
-
-# Root options currently will use no root by default. wItS
-#ROOT_METHOD=magisk
-#ROOT_METHOD=su -- to compile builds with default su provided by LineageOS
-# Without any of these flags the builds will be unrooted and won't include Adaway & KernelAdiutor
-
+PRODUCT_CHARACTERISTICS := nosdcard
 # Use the latest approved GMS identifiers unless running a signed build
-ifneq ($(SIGN_BUILD),true)
+PRODUCT_GMS_CLIENTID_BASE := android-leeco
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=Letv/Le1s_CN/X3:6.0/DBXCNOP5902302082S/1486532099:user/release-keys
-endif
+    BUILD_FINGERPRINT=Letv/Le1s_CN/X3:6.0/DBXCNOP5902302082S/1486532099:user/release-keys \
+    PRIVATE_BUILD_DESC="full_x500-user 6.0 DHXOSOP5801911241S 1479990092 release-keys"
